cedf5dd287e50c6c5fdb8c0a5c86cd399ab4049d
Salil Luley <salilluley@Salils-MacBook-Pro.local>
2026-01-06 16:09:18 +0000

    added problem

---
+++ src/Array/PlusOne.java
@@ -0,0 +1,26 @@
+package Array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class PlusOne {
+    public static void main(String[] args) {
+        int[] digits = new int[]{9,8,9};
+        System.out.println(Arrays.toString(plusOne(digits)));
+    }
+
+    public static int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] != 9) {
+                digits[i]++;
+                return digits;
+            }
+
+            digits[i] = 0;
+        }
+
+        digits = new int[digits.length+1];
+        digits[0] = 1;
+        return digits;
+    }
+}
